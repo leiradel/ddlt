a60bb7ef0ec509760ce70f6f93c5efb1a75c0fd3
Andre Leiradella <andre@leiradella.com>
2017-12-14 10:48:43 +0000

    Made the BASIC parsing example an unit test

--- example/test_bas.ddl
+++ example/test_bas.ddl
@@ -1,22 +1,6 @@
-' The weapons available in the game
-enum Weapons as int
-  kFist
-  kChainsaw
-  kPistol
-  kShotgun
-  kChaingun
-  kRocketLauncher
-  kPlasmaGun
-  kBFG9000
-end enum
-
-rem The player
-structure Hero
-  name as string = "John ""Hero"" Doe"
-  health as int = 100%
-  armour as int = &h0
-  speed as float = 14.3
-  isAlive = [{
-    return true;
-  }]
-end structure
+' Line comment
+rem Line comment
+REM Line comment
+id Id iD ID _id id_ _
+0 &habcd &HABCD &o123 &O123 &b01 &B01 123 1. .1 1.1 1.f .1f 1.1f 1.e1 .1e1 1.1e1 1.e1f .1e1f 1.1e1f 1.r 1.d 1.@ 1.! 1.# 1s 1us 1i 1ui 1l 1ul 1% 1& 1UL 1#
+"" """" """a""" "a""" """a"
--- example/test_bas.lua
+++ example/test_bas.lua
@@ -31,14 +31,15 @@ local parse = function(file)
   return tokens
 end
 
-local template = [[
-/*! local tkfmt = '%-' .. args.max .. 's' */
+local template = [===[
+local expected = {
 /*! for i = 1, #args do */
 /*!  local la = args[ i ] */
-line = /*= string.format('%3d', la.line) */ token = /*= string.format(tkfmt, la.token) */ lexeme = /*= la.lexeme */
+  {line = /*= la.line */, token = [[/*= la.token */]], lexeme = [[/*= la.lexeme */]]},
 /*! end */
-]]
-
+}
+]===]
+  
 return function(args)
   if #args ~= 1 then
     error('missing input file\n')
@@ -52,4 +53,82 @@ return function(args)
 
   res = table.concat(res):gsub('\n+', '\n')
   io.write(res)
+
+  local expected = {
+    {line = 4, token = [[<id>]], lexeme = [[id]]},
+    {line = 4, token = [[<id>]], lexeme = [[Id]]},
+    {line = 4, token = [[<id>]], lexeme = [[iD]]},
+    {line = 4, token = [[<id>]], lexeme = [[ID]]},
+    {line = 4, token = [[<id>]], lexeme = [[_id]]},
+    {line = 4, token = [[<id>]], lexeme = [[id_]]},
+    {line = 4, token = [[<id>]], lexeme = [[_]]},
+    {line = 5, token = [[<decimal>]], lexeme = [[0]]},
+    {line = 5, token = [[<hexadecimal>]], lexeme = [[&habcd]]},
+    {line = 5, token = [[<hexadecimal>]], lexeme = [[&HABCD]]},
+    {line = 5, token = [[<octal>]], lexeme = [[&o123]]},
+    {line = 5, token = [[<octal>]], lexeme = [[&O123]]},
+    {line = 5, token = [[<binary>]], lexeme = [[&b01]]},
+    {line = 5, token = [[<binary>]], lexeme = [[&B01]]},
+    {line = 5, token = [[<decimal>]], lexeme = [[123]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.]]},
+    {line = 5, token = [[<float>]], lexeme = [[.1]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.1]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.f]]},
+    {line = 5, token = [[<float>]], lexeme = [[.1f]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.1f]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.e1]]},
+    {line = 5, token = [[<float>]], lexeme = [[.1e1]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.1e1]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.e1f]]},
+    {line = 5, token = [[<float>]], lexeme = [[.1e1f]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.1e1f]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.r]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.d]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.@]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.!]]},
+    {line = 5, token = [[<float>]], lexeme = [[1.#]]},
+    {line = 5, token = [[<decimal>]], lexeme = [[1s]]},
+    {line = 5, token = [[<decimal>]], lexeme = [[1us]]},
+    {line = 5, token = [[<decimal>]], lexeme = [[1i]]},
+    {line = 5, token = [[<decimal>]], lexeme = [[1ui]]},
+    {line = 5, token = [[<decimal>]], lexeme = [[1l]]},
+    {line = 5, token = [[<decimal>]], lexeme = [[1ul]]},
+    {line = 5, token = [[<decimal>]], lexeme = [[1%]]},
+    {line = 5, token = [[<decimal>]], lexeme = [[1&]]},
+    {line = 5, token = [[<decimal>]], lexeme = [[1UL]]},
+    {line = 5, token = [[<float>]], lexeme = [[1#]]},
+    {line = 6, token = [[<string>]], lexeme = [[""]]},
+    {line = 6, token = [[<string>]], lexeme = [[""""]]},
+    {line = 6, token = [[<string>]], lexeme = [["""a"""]]},
+    {line = 6, token = [[<string>]], lexeme = [["a"""]]},
+    {line = 6, token = [[<string>]], lexeme = [["""a"]]},
+    {line = 7, token = [[<eof>]], lexeme = [[<eof>]]},
+  }
+
+  assert(#tokens == #expected, 'Wrong number of tokens produced')
+  
+  for i = 1, #tokens do
+    local la1 = tokens[i]
+    local la2 = expected[i]
+
+    local msg1 = string.format('{line = %d, token = [[%s]], lexeme = [[%s]]}', la1.line, la1.token, la1.lexeme)
+    local msg2 = string.format('{line = %d, token = [[%s]], lexeme = [[%s]]}', la2.line, la2.token, la2.lexeme)
+
+    if la1.line ~= la2.line then
+      io.write(string.format('Lines are different in\n  %s\n  %s\n', msg1, msg2))
+      os.exit(1)
+    end
+
+    if la1.token ~= la2.token then
+      io.write(string.format('Tokens are different in\n  %s\n  %s\n', msg1, msg2))
+      os.exit(1)
+    end
+    
+    if la1.lexeme ~= la2.lexeme then
+      io.write(string.format('Lexemes are different in\n  %s\n  %s\n', msg1, msg2))
+      os.exit(1)
+    end
+  end
+
+  io.write('-- tests passed\n')
 end
