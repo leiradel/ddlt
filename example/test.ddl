38f9af4de5e6419ad33adc27d1d977c130da418e
Andre Leiradella <andre@leiradella.com>
2017-12-14 13:09:47 +0000

    Added the possibility to specify the arrays of accepted symbols and keywords when creating the lexer

---
+++ example/test.ddl
@@ -0,0 +1,22 @@
+// The weapons available in the game
+enum Weapons {
+  kFist,
+  kChainsaw,
+  kPistol,
+  kShotgun,
+  kChaingun,
+  kRocketLauncher,
+  kPlasmaGun,
+  kBFG9000
+};
+
+/* The player */
+struct Hero {
+  string name = "John \"Hero\" Doe";
+  int health = 100;
+  int armour = 0x0;
+  float speed = 14.3;
+  isAlive = [{
+    return true;
+  }]
+};
--- src/boot.lua
+++ src/boot.lua
@@ -22,5 +22,54 @@ return function(args)
   args[1] = ddlt.realpath(args[1])
 
   local main = assert(loadfile(args[1], 't'))()
+
+  local newLexer = ddlt.newLexer
+
+  ddlt.newLexer = function(options)
+    if not options.isSymbol and options.symbols then
+      local symbols = {}
+
+      for i = 1, #options.symbols do
+        symbols[options.symbols[i]] = true
+      end
+
+      options.isSymbol = function(symbol)
+        return symbols[symbol]
+      end
+    end
+
+    local lexer, err = newLexer(options)
+
+    if not lexer then
+      return nil, err
+    end
+
+    if options.keywords then
+      local keywords = {}
+
+      for i = 1, #options.keywords do
+        keywords[options.keywords[i]] = true
+      end
+
+      return {
+        next = function(self, la)
+          local la, err = lexer:next(la)
+
+          if not la then
+            return nil, err
+          end
+
+          if la.token == '<id>' and keywords[la.lexeme] then
+            la.token = string.format('[%s]', la.lexeme)
+          end
+
+          return la
+        end
+      }
+    else
+      return lexer
+    end
+  end
+
   main(args)
 end
