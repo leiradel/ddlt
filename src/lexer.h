7e4a7dbdc53b4c7605e54fdd493018fb5c48ded9
Andre Leiradella <andre@leiradella.com>
2020-11-28 19:20:05 +0000

    ddlt has moved to luamods

--- src/lexer.h
+++
@@ -1,8 +0,0 @@
-#ifndef LEXER_H
-#define LEXER_H
-
-#include <lua.h>
-
-int l_newLexer(lua_State* L);
-
-#endif /* LEXER_H */
--- src/templ.h
+++
@@ -1,8 +0,0 @@
-#ifndef TEMPL_H
-#define TEMPL_H
-
-#include <lua.h>
-
-int l_newTemplate(lua_State* L);
-
-#endif /* TEMPL_H */
